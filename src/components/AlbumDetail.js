f098c3fb85841e026ee20f6c83e057a6cd4f9abd
Sam G. Zun <zunsamuel97@gmail.com>
2018-08-12 22:21:53 -0500

    added album images and scrollview

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, ScrollView } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title } = album;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    thumbanilStyle,
+    thumbanilContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <Text>{title}</Text>
+        <View style={thumbanilContainerStyle}>
+          <Image style={thumbanilStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbanilStyle: {
+    height: 50,
+    width: 50
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  thumbanilContainerStyle: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
